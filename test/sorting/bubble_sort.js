7a51df4b28d43bcd2d1b5773aa90e3d71de2e721
Justin Molineaux <justin.molineaux@gmail.com>
2016-10-29 23:02:37 -0400

    Adds bubble sort.

---
+++ test/sorting/bubble_sort.js
@@ -0,0 +1,36 @@
+var should     = require('chai').should(),
+    bubbleSort = require('../../sorting/bubble_sort.js');
+
+describe('#bubbleSort', function() {
+
+  it('should sort numeric arrays', function() {
+    var unsorted = [5, 9, 1, 7, 6, 0, 3],
+        sorted   = [0, 1, 3, 5, 6, 7, 9];
+    bubbleSort(unsorted).should.eql(sorted);
+  });
+
+  it('should sort alphabetic arrays', function() {
+    var unsorted = ['banana', 'apple', 'tomato', 'carrot'],
+        sorted   = ['apple', 'banana', 'carrot', 'tomato'];
+    bubbleSort(unsorted).should.eql(sorted);
+  })
+
+  it('should sort uniform-value arrays', function() {
+    var unsorted = [1, 1, 1, 1, 1, 1, 1, 1, 1],
+        sorted   = [1, 1, 1, 1, 1, 1, 1, 1, 1];
+    bubbleSort(unsorted).should.eql(sorted);
+  });
+
+  it('should sort already-sorted arrays', function() {
+    var unsorted = [1, 2, 3, 4, 5, 6, 7, 8],
+        sorted   = [1, 2, 3, 4, 5, 6, 7, 8];
+    bubbleSort(unsorted).should.eql(sorted);
+  });
+
+  it('should sort reverse-sorted arrays', function() {
+    var unsorted = [8, 7, 6, 5, 4, 3, 2, 1],
+        sorted   = [1, 2, 3, 4, 5, 6, 7, 8];
+    bubbleSort(unsorted).should.eql(sorted);
+  });
+
+});
