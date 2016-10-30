4359fea5930fd1e3d7cc22d07ebf00c283b5ba35
Justin Molineaux <justin.molineaux@gmail.com>
2016-10-29 21:43:59 -0400

    Adds sentinel-free version of merge-sort.

--- sorting/merge_sort.js
+++ sorting/merge_sort.js
@@ -1,3 +1,30 @@
-var mergeSort = function() {};
+var mergeSort = function( A, p, r ) {
+  if ( p == null ) p = 0;
+  if ( r == null ) r = A.length - 1;
+
+  if ( p < r ) {
+    var q = Math.ceil( (p + r) / 2 );
+    mergeSort( A, p, q - 1 );
+    mergeSort( A, q, r );
+    merge( A, p, q, r );
+  }
+  return A;
+}
+
+var merge = function( A, p, q, r ) {
+  var L = A.slice( p, q ),
+      R = A.slice( q, r + 1 ),
+      i = j = 0;
+
+  // Using Infinity as our sentinel does not allow us to compare strings.
+  // L.push( Infinity );
+  // R.push( Infinity );
+
+  // Instead, we'll just stop picking from L & R when i or j goes out of bounds.
+  for ( var k = p; k <= r; k++ ) {
+    if ( L[i] <= R[j] || R[j] == null ) A[k] = L[i++];
+    else                                A[k] = R[j++];
+  }
+}
 
 module.exports = mergeSort;
